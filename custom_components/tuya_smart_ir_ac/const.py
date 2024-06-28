048467d15498aeef161a5faada354d721fad986a
EnzoD86 <61162811+EnzoD86@users.noreply.github.com>
2024-06-28 02:32:50 +0200

    Update const.py

--- custom_components/tuya_smart_ir_ac/const.py
+++ custom_components/tuya_smart_ir_ac/const.py
@@ -1,10 +1,23 @@
-from homeassistant.components.climate.const import HVACMode
+from homeassistant.components.climate import (
+    FAN_AUTO,
+    FAN_LOW,
+    FAN_MEDIUM,
+    FAN_HIGH,
+    HVACMode
+)
 
-VALID_MODES = {
+TUYA_HVAC_MODES = {
     "0": HVACMode.COOL,
     "1": HVACMode.HEAT,
     "2": HVACMode.AUTO,
     "3": HVACMode.FAN_ONLY,
     "4": HVACMode.DRY,
     "5": HVACMode.OFF,
+}
+
+TUYA_FAN_MODES = {
+    "0": FAN_AUTO,
+    "1": FAN_LOW,
+    "2": FAN_MEDIUM,
+    "3": FAN_HIGH
 }
